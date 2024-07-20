419ddb3ebde1d86a060beb9f1ea9be9404b34887
Mohamed Mosaad <mohamedmosaad8841@gamil.com>
2024-07-20 21:09:08 +0200

    this is the third time i use git

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{int a,i,f,fact=1;
+printf("enter your number");
+scanf("%d",&a);
+i=1;
+
+while(a>0)
+{f=a*i;
+fact=fact*f;
+    a--;
+}
+printf("the result is %d",fact);
+    return 0;
+}
